f5381e7fd673686b46de0f72a7cc0e415fe3d506
xiaomei <v-xiaomeixie@microsoft.com>
2021-06-03 08:37:13 -0700

    Add parallel chart.

--- index.js
+++ index.js
@@ -14,6 +14,43 @@ var wavesurfer = WaveSurfer.create({
     height: 150
 });
 
+var trace = {
+  type: 'parcoords',
+  line: {
+    color: 'blue'
+  },  
+  dimensions: [{
+    range: [0, 10],
+    constraintrange: [3, 4],
+    label: 'Cry',
+    values: [4,5],
+	tickvals: [0,4,5,10],
+    ticktext: ['0','28-May','29-May','10']
+  }, {    
+    range: [0,10],
+    label: 'Laugh',
+    values: [7,6],
+    tickvals: [0,7,6,10],
+	ticktext: ['0','28-May','29-May','10']
+  }, {
+    range: [0, 10],
+    label: 'Mumble',
+    values: [10,5],
+    tickvals: [0,10,5,10],
+    ticktext: ['0','28-May','29-May','10']
+  }, {
+    range: [0, 10],
+    label: 'Yell',
+    values: [0,0],
+	tickvals: [0,0,0,10],
+    ticktext: ['0','28-May','29-May','10']
+  }]
+};
+
+var data = [trace];
+
+Plotly.newPlot('myDiv', data);
+
 // video and audio visualization update triggered by button click
 async function updateVideo(expression) {
     var videoData = "static/video/" + expression + ".mp4";
