bdb800a4bae4f267730f3a234360c16b055583ee
xiaomei <v-xiaomeixie@microsoft.com>
2021-06-04 00:19:51 -0700

    Update pallalChart, problem is don't show on web

--- js/parallelcoordinates.js
+++ js/parallelcoordinates.js
@@ -31,6 +31,22 @@
 //     ticktext: ['0','28-May','29-May','10']
 //   }]
 // };
+d3.csv("/data/baby.csv").then(pallalCharts);
+
+var dataSet = d3.csv("/data/baby.csv");
+var yesterday ;
+var today;
+
+function dayData(data, date, isToday) {
+    var ret = [];
+    dt = data.filter(x => x['day'] == date)[0];
+	ret.push(dt['Cry'], dt['Laugh'], dt['Mumble'], dt['Yell']);
+    return ret;
+}
+
+var emotion = ['0','Cry', 'Laugh', 'Mumble', 'Yell', '20'];
+yesterday = dayData(dataSet, 'yesterday');
+today = dayData(dataSet, 'today');
 
 var trace = {
   type: 'parcoords',
@@ -40,18 +56,19 @@ var trace = {
   
   dimensions: [{
     range: [0, 20],
-    label: 'Yesterday: 28-May',
-    values: [8,17,16,0],
+    label: 'Yesterday',
+    values: yesterday, //[8,17,16,0],
 	  tickvals: [0,8,17,16,0,20],
-    ticktext: ['0','Cry','Laugh','Mumble','Yell','20']
+    ticktext: emotion
   }, {    
     range: [0, 20],
-    label: 'Today: 29-May',
-    values: [12,16,13,2],
+    label: 'Today',
+    values: today,//[12,16,13,2],
     tickvals: [0,12,16,13,2,20],
-	ticktext: ['0','Cry','Laugh','Mumble','Yell','20']
+	ticktext: emotion
   }]
 };
+
 
 var data = [trace]
 
